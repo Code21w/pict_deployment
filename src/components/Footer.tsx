377d1da81000ba06c90a4d31b844b8d7f8ec0fb2
[AI10] 김재근 <jskim331933@naver.com>
2024-07-20 16:28:40 +0900

    지구polygon 회전

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <footer className='flex justify-between w-full border-t-[1px]'>
+      <span className='sr-only'>차징</span>
+      <div className='flex flex-1 place-self-center text-xs'>
+        © 2024 PicT Inc. All rights reserved.
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
