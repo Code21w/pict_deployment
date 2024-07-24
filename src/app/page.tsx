60eda318b6dcf17b7a81372d94547d8b344ca01b
[AI10] 김재근 <jskim331933@naver.com>
2024-07-24 23:14:45 +0900

    주석 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react'; // useState 추가
+import React from 'react'; // useState 추가
 import Image from 'next/image';
 
 import CloudAnimation from '@/components/main/Cloud';
@@ -43,7 +43,7 @@ function Main() {
   return (
     <main className='flex min-h-screen items-center justify-between'>
       <div className='w-full h-screen'>
-        <div className='max-[1200px]:mt-[10%] mx-[200px] lg:mx-[250px] min-[1500px]:-mb-[150px] flex flex-col justify-between min-w-[460px]'>
+        <div className='mx-[200px] lg:mx-[250px] max-[1200px]:mt-[10%] min-[1500px]:-mb-[150px] flex flex-col justify-between min-w-[460px]'>
           <div
             className={`relative text-8xl min-[1200px]:text-[144px] min-[1500px]:text-[192px] font-['Cafe24Moyamoya-Face-v1.0'] -py-3`}
           >
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,6 @@ import Link from 'next/link';
 import Join from './Join';
 import Login from './Login';
 
-/** image */
-// import { Menu } from 'lucide-react';
-// import logo from '../../assets/images/logo.png';
-
 function Header() {
   return (
     <header className='z-10 sticky top-0 flex h-16 items-center gap-4 px-4 md:px-6 flex justify-between'>
@@ -25,13 +21,7 @@ function Header() {
         <Link href={'/'} className='text-foreground transition-colors hover:text-muted'>
           메인2
         </Link>
-        {/* <Link href={'/'} className='text-muted-foreground transition-colors hover:text-foreground'>
-          로그인
-        </Link> */}
         <Login />
-        {/* <Link href={'/'} className='text-muted-foreground transition-colors hover:text-foreground'>
-          회원가입
-        </Link> */}
         <Join />
       </nav>
     </header>
