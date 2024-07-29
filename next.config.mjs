3cc691fa0998d4825814dc8392b8357c45027151
21w <hansu0201@naver.com>
2024-07-29 22:48:49 +0900

    프론트 프록시 적용

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:3000/api/:path*', // The :path parameter is used here so will not be automatically passed in the query
+      },
+    ];
+  },
+};
+
 export default nextConfig;
