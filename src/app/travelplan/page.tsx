9b4433b6eb054d2ede272384855deaaf5aa8c8f3
[AI10] 김재근 <jskim331933@naver.com>
2024-08-01 15:21:13 +0900

    0801 자잘한 버그 fix

--- src/app/travelplan/page.tsx
+++ src/app/travelplan/page.tsx
@@ -10,17 +10,25 @@ import {
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select';
+import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 function TravelPlan() {
   const form = useForm();
-  const day = 3; //기간 선택한거에 따라 변동
+  const [period, setPeriod] = useState(3);
+  const [areaName, setAreaName] = useState('광명');
+  const [rcPlace, setRcPlace] = useState(['']);
+  //페이지 첫 렌더링 시 ai가 생성해준 데이터로 설정
+  useEffect(() => {
+    setPeriod(4), setAreaName('default'), setRcPlace(['해수욕장', '절', '샘플3']);
+  }, []);
+
   return (
     <div className='border-solid border-2 flex h-screen'>
       <div className='border-solid border-2 flex max-h-full relative'>
         <div>
           <div className='flex flex-col items-center'>
-            <div className='m-5 mt-10'>광명으로 가는 나만의 트레블리스트</div>
-            <div className='mt-5 text-lg'>광명으로의 여행을 계획해볼까요?</div>
+            <div className='mb-5 mt-10 text-center'>{`${areaName}으로 가는 나만의 트레블리스트`}</div>
+            <div className='mt-5 text-lg text-center'>{`${areaName}으로의 여행을 계획해볼까요?`}</div>
           </div>
           <Card className='rounded-lg border bg-cyan-500 text-card-foreground shadow-sm w-[250px] h-20 m-[20px]'>
             <Form {...form}>
@@ -34,13 +42,24 @@ function TravelPlan() {
                         <FormControl>
                           <div>
                             <SelectTrigger className='pl-[35px] w-[100px]'>
-                              <SelectValue placeholder='날짜' />
+                              <SelectValue placeholder={`${period}일`} />
                             </SelectTrigger>
                             <SelectContent>
-                              <SelectItem value='1'>1일</SelectItem>
-                              <SelectItem value='2'>2일</SelectItem>
-                              <SelectItem value='3'>3일</SelectItem>
-                              <SelectItem value='4'>4일</SelectItem>
+                              <SelectItem value='1' onClick={() => setPeriod(1)}>
+                                1일
+                              </SelectItem>
+                              <SelectItem value='2' onClick={() => setPeriod(2)}>
+                                2일
+                              </SelectItem>
+                              <SelectItem value='3' onClick={() => setPeriod(3)}>
+                                3일
+                              </SelectItem>
+                              <SelectItem value='4' onClick={() => setPeriod(4)}>
+                                4일
+                              </SelectItem>
+                              <SelectItem value='5' onClick={() => setPeriod(5)}>
+                                5일
+                              </SelectItem>
                             </SelectContent>
                           </div>
                         </FormControl>
@@ -60,39 +79,15 @@ function TravelPlan() {
             <PlaceCategory />
           </div>
           <div className='list_container flex flex-col border-solid border-2 box-content max-h-[480px] overflow-auto'>
-            백에서 받아올 장소 정보
-            <div className='list_component flex items-center border-solid border-2 w-full min-h-[100px]'>
-              <div className='image_component border-solid border-2 rounded-md w-[64px] h-[64px] mx-[15px]'></div>
-              <div>장소이름</div>
-            </div>
-            <div className='list_component flex items-center border-solid border-2 w-full min-h-[100px]'>
-              <div className='image_component border-solid border-2 rounded-md w-[64px] h-[64px] mx-[15px]'></div>
-              <div>장소이름</div>
-            </div>
-            <div className='list_component flex items-center border-solid border-2 w-full min-h-[100px]'>
-              <div className='image_component border-solid border-2 rounded-md w-[64px] h-[64px] mx-[15px]'></div>
-              <div>장소이름</div>
-            </div>
-            <div className='list_component flex items-center border-solid border-2 w-full min-h-[100px]'>
-              <div className='image_component border-solid border-2 rounded-md w-[64px] h-[64px] mx-[15px]'></div>
-              <div>장소이름</div>
-            </div>
-            <div className='list_component flex items-center border-solid border-2 w-full min-h-[100px]'>
-              <div className='image_component border-solid border-2 rounded-md w-[64px] h-[64px] mx-[15px]'></div>
-              <div>장소이름</div>
-            </div>
-            <div className='list_component flex items-center border-solid border-2 w-full min-h-[100px]'>
-              <div className='image_component border-solid border-2 rounded-md w-[64px] h-[64px] mx-[15px]'></div>
-              <div>장소이름</div>
-            </div>
-            <div className='list_component flex items-center border-solid border-2 w-full min-h-[100px]'>
-              <div className='image_component border-solid border-2 rounded-md w-[64px] h-[64px] mx-[15px]'></div>
-              <div>장소이름</div>
-            </div>
-            <div className='list_component flex items-center border-solid border-2 w-full min-h-[100px]'>
-              <div className='image_component border-solid border-2 rounded-md w-[64px] h-[64px] mx-[15px]'></div>
-              <div>장소이름</div>
-            </div>
+            {rcPlace.map((item, idx) => (
+              <div
+                key={idx}
+                className='list_component flex items-center border-solid border-2 w-full min-h-[100px]'
+              >
+                <div className='image_component border-solid border-2 rounded-md w-[64px] h-[64px] mx-[15px]'></div>
+                <div>{item}</div>
+              </div>
+            ))}
           </div>
         </div>
         <div className='border-solid border-2 w-screen h-screen rounded-md border max-h-full overflow-auto relative'>
