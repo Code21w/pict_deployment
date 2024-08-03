5384f4afa2cdcfbbacd91dfde2643f66f80f8460
[AI10] 김재근 <jskim331933@naver.com>
2024-08-03 23:28:16 +0900

    feature: main 스크린 사이즈에 따른 이미지 업로드 컴포넌트 위치 조정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,9 @@ import fetchLocationInfo from '@/api/fetchLocationInfo';
 import generateAndStoreImage from '@/api/generateAndStoreImage';
 import imageIcon from '@/assets/images/image_icon.png';
 import CloudAnimation from '@/components/main/Cloud';
-// import { DialogDemo } from '@/components/resultmodal/ResultModal';
+import { DialogDemo } from '@/components/resultModal/ResultModal';
 import { Dialog } from '@/components/ui/dialog';
+import useWindowSize from '@/hooks/useWindowSize';
 
 import AirplaneAnimation from '@/components/main/Airplane';
 import Globe from '@/components/main/Globe';
@@ -26,6 +27,7 @@ function Main() {
   const [isUploadedImageVisible, setIsUploadedImageVisible] = useState(false);
   const [locationInfo, setLocationInfo] = useState('');
 
+  useWindowSize();
   const onDrop = useCallback((acceptedFiles: File[], fileRejections: any[]) => {
     // 파일 형식 오류가 있는 경우
     if (fileRejections.length > 0) {
@@ -101,83 +103,95 @@ function Main() {
       setLoading(false); // 에러가 발생해도 로딩 상태 해제 추후 출력 페이지 작성
     }
   };
-
-  return (
-    <main className='flex min-h-screen items-center justify-between'>
-      <div className='w-full h-screen'>
-        <div className='mx-[200px] lg:mx-[250px] max-[1200px]:mt-[10%] min-[1500px]:-mb-[150px] flex flex-col justify-between min-w-[460px]'>
-          <div
-            className={`relative text-8xl min-[1200px]:text-[144px] min-[1500px]:text-[192px] font-['Cafe24Moyamoya-Face-v1.0'] -py-3`}
-          >
-            여기서
-          </div>
-          <div
-            className={`self-end relative text-8xl min-[1200px]:text-[144px] min-[1500px]:text-[192px] font-['Cafe24Moyamoya-Face-v1.0'] -my-3`}
-          >
-            세계속으로
-          </div>
-        </div>
-
-        <div className='relative z-1 ml-[50px]'>
-          <Globe />
-        </div>
-        <div className='fixed z-2 -mt-[600px]'>
-          <AirplaneAnimation />
-        </div>
-        <div className='relative z-3 -mt-[800px]'>
-          <CloudAnimation />
-        </div>
-        <div className='relative z-4 max-[1200px]:-mt-[300px] -mt-[400px] mb-[150px] ml-[40%] flex flex-col w-[500px] h-[100px]'>
-          <div className='flex'>
-            <div className='text-lg'>해외 이미지를 넣으면&nbsp;</div>
-            <b className='text-lg'>국내에서 가장 비슷한 곳</b>
-            <div className='text-lg'>을 찾아드려요!</div>
-          </div>
-          <div
-            {...getRootProps()}
-            className={`flex items-center bg-gray-100 rounded-3xl shadow-md justify-center mt-[10px] ${
-              isDragActive ? 'bg-gray-200' : 'bg-gray-100'
-            }`}
-          >
-            <input {...getInputProps()} className='w-[300px] h-[100px] bg-transparent pl-[10px]' />
-            {!image && <Image src={imageIcon} alt='UploadImageIcon.png' />}
-            {image && (
-              <img
-                src={image}
-                alt='Uploaded Preview'
-                className='w-[300px] h-[100px] object-cover'
-              />
-            )}
+  if (typeof window !== 'undefined') {
+    const windowHeight = window.innerHeight;
+    return (
+      <main className='flex min-h-screen items-center justify-between'>
+        <div className='w-full h-screen'>
+          <div className='mx-[200px] lg:mx-[250px] max-[1200px]:mt-[10%] min-[1500px]:-mb-[150px] flex flex-col justify-between min-w-[460px]'>
+            <div
+              className={`relative text-8xl min-[1200px]:text-[144px] min-[1500px]:text-[192px] font-['Cafe24Moyamoya-Face-v1.0'] -py-3`}
+            >
+              여기서
+            </div>
+            <div
+              className={`self-end relative text-8xl min-[1200px]:text-[144px] min-[1500px]:text-[192px] font-['Cafe24Moyamoya-Face-v1.0'] -my-3`}
+            >
+              세계속으로
+            </div>
           </div>
 
-          <div>
-            <button
-              onClick={handleButtonClick}
-              disabled={!file}
-              className={`w-[500px] h-[50px] rounded-3xl shadow-md mt-[10px] ${
-                file ? 'bg-cyan-300 active:bg-cyan-400' : 'bg-gray-300 cursor-not-allowed'
+          <div className='relative z-1 ml-[50px]'>
+            <Globe />
+          </div>
+          <div className='fixed z-2 -mt-[600px]'>
+            <AirplaneAnimation />
+          </div>
+          <div className='relative z-3 -mt-[800px]'>
+            <CloudAnimation />
+          </div>
+          <div
+            className='relative z-4 -mt-[300px] mb-[150px] ml-[40%] flex flex-col w-[500px] h-[100px] big-screen-size small-screen-size'
+            style={
+              {
+                '--inner-height': `${windowHeight}px`,
+              } as React.CSSProperties
+            }
+          >
+            <div className='flex'>
+              <div className='text-lg'>해외 이미지를 넣으면&nbsp;</div>
+              <b className='text-lg'>국내에서 가장 비슷한 곳</b>
+              <div className='text-lg'>을 찾아드려요!</div>
+            </div>
+            <div
+              {...getRootProps()}
+              className={`flex items-center bg-gray-100 rounded-3xl shadow-md justify-center mt-[10px] ${
+                isDragActive ? 'bg-gray-200' : 'bg-gray-100'
               }`}
             >
-              {file ? '여기랑 비슷한 곳 찾아주세요!' : '이미지를 업로드 해주세요!'}
-            </button>
-            {errorMessage && <div className='text-red-500 mt-2'>{errorMessage}</div>}
+              <input
+                {...getInputProps()}
+                className='w-[300px] h-[100px] bg-transparent pl-[10px]'
+              />
+              {!image && <Image src={imageIcon} alt='UploadImageIcon.png' />}
+              {image && (
+                <img
+                  src={image}
+                  alt='Uploaded Preview'
+                  className='w-[300px] h-[100px] object-cover'
+                />
+              )}
+            </div>
+
+            <div>
+              <button
+                onClick={handleButtonClick}
+                disabled={!file}
+                className={`w-[500px] h-[50px] rounded-3xl shadow-md mt-[10px] ${
+                  file ? 'bg-cyan-300 active:bg-cyan-400' : 'bg-gray-300 cursor-not-allowed'
+                }`}
+              >
+                {file ? '여기랑 비슷한 곳 찾아주세요!' : '이미지를 업로드 해주세요!'}
+              </button>
+              {errorMessage && <div className='text-red-500 mt-2'>{errorMessage}</div>}
+            </div>
+            <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+              <DialogDemo
+                responseImage={responseImage}
+                location={location}
+                locationInfo={locationInfo}
+                similarity={similarity}
+                isUploadedImageVisible={isUploadedImageVisible}
+                setIsUploadedImageVisible={setIsUploadedImageVisible}
+                image={image}
+                loading={loading}
+              />
+            </Dialog>
           </div>
-          <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
-            {/* <DialogDemo
-              responseImage={responseImage}
-              location={location}
-              locationInfo={locationInfo}
-              similarity={similarity}
-              isUploadedImageVisible={isUploadedImageVisible}
-              setIsUploadedImageVisible={setIsUploadedImageVisible}
-              image={image}
-              loading={loading}
-            /> */}
-          </Dialog>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  }
 }
 
 export default Main;
