d276a95cce63dbdd8c688c87fcc98869127b1a1c
[AI10] 김재근 <jskim331933@naver.com>
2024-08-03 15:53:17 +0900

    찜 장소리스트 컴퍼넌트 제작중

--- src/app/travelplan/page.tsx
+++ src/app/travelplan/page.tsx
@@ -1,9 +1,8 @@
 'use client';
-import ImageIcon from '@/assets/images/image_icon.png';
 import ExpandButton from '@/components/travel_plan/ExpandButton';
 import PlaceCategory from '@/components/travel_plan/PlaceCategory';
+import PlaceListBlock from '@/components/travel_plan/PlaceListBlock';
 import TravelPlanCheckButton from '@/components/travel_plan/TravelPlanCheckButton';
-import Image from 'next/image';
 import { useEffect, useState } from 'react';
 function TravelPlan() {
   const [period, setPeriod] = useState(3);
@@ -36,25 +35,16 @@ function TravelPlan() {
 
           <div className='list_container flex flex-col border-solid border-2 box-content overflow-auto'>
             {rcPlace.map((item, idx) => (
-              <div
-                key={idx}
-                className='list_component flex items-center border-solid border-2 w-full min-h-[100px] w-[300px]'
-              >
-                <div className='image_component border-solid border-2 rounded-md w-[64px] h-[64px] mx-[15px]'>
-                  <Image
-                    src={ImageIcon}
-                    alt='recommended_place'
-                    className='object-scale-down w-[64px] h-[64px]'
-                  ></Image>
-                </div>
+              <PlaceListBlock key={idx}>
                 <div>{item}</div>
                 <div className='absolute right-3'>
                   <TravelPlanCheckButton changeSelectCountFunction={changeSelectCount} />
                 </div>
-              </div>
+              </PlaceListBlock>
             ))}
           </div>
         </div>
+        {/* // */}
         <div
           id='temp_place_edit_container'
           className='relative border-solid border-2 border-green-500 w-[120px]'
@@ -62,11 +52,19 @@ function TravelPlan() {
           <div>
             <div className='text-4xl'>{placeSelectCount}</div>
           </div>
+          {/*  */}
+          <div>
+            <PlaceListBlock>
+              <div>333</div>
+            </PlaceListBlock>
+          </div>
+          {/*  */}
           <div>
             <ExpandButton />
           </div>
           {/* width: 120px ~ 400px */}
         </div>
+        {/* // */}
         <div className='border-solid border-2 border-red-500 w-screen h-screen rounded-md border max-h-full overflow-auto relative'>
           <div className='flex rounded-md bg-gray-500/50 items-center m-[30px] -mb-[10px]'>
             <div className='rounded-md m-[5px] active:bg-white'>Travel Plan</div>
