28a4088dc01c64b044bdbf236c7b9cb241aaa21a
21w <hansu0201@naver.com>
2024-08-09 01:07:28 +0900

    error 수정

--- src/app/verify/email/[token]/page.tsx
+++ src/app/verify/email/[token]/page.tsx
@@ -1,24 +1,19 @@
-"use client"; 
+'use client';
 
 import VerifyEmail from '../../../../components/login-join/VerifyEmail';
 
-
 interface Props {
   params: {
-    token: string; 
+    token: string;
   };
 }
 
 const SubPage: React.FC<Props> = ({ params }) => {
-  const { token } = params; 
+  const { token } = params;
 
   return (
     <main className='w-full h-screen'>
-      {token ? (
-        <VerifyEmail token={token} />
-      ) : (
-        <p>Loading...</p> 
-      )}
+      {token ? <VerifyEmail token={token} /> : <p>Loading...</p>}
     </main>
   );
 };
