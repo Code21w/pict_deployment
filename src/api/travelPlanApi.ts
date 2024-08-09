e0aef0fb3f61e214bdd066e2c6d601fadbb62369
[AI10] 김재근 <jskim331933@naver.com>
2024-08-10 03:42:21 +0900

    로그인 안할시 경고창 on

--- src/api/travelPlanApi.ts
+++ src/api/travelPlanApi.ts
@@ -1,7 +1,9 @@
 import { instance } from './instance';
 
 export const fetchPlace = async (id: string, category: string) => {
-  const result = await instance.get(`/api/planner_market?sigungu_id=${id}&category=${category}`);
+  const result = await instance.get(`/api/planner_market?sigungu_id=${id}&category=${category}`, {
+    withCredentials: true,
+  });
 
   return result;
 };
