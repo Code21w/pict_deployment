7a34fcb1f229f121462623f58c6e4fe3affa1a86
Code21w <167353588+Code21w@users.noreply.github.com>
2024-08-10 11:54:17 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,3 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   images: {
@@ -14,20 +13,6 @@ const nextConfig = {
     return config;
   },
   
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this with your actual origin
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ],
-      },
-    ];
-  },
 
   experimental: {
     turbo: {
@@ -40,5 +25,4 @@ const nextConfig = {
     },
   },
 };
-
 export default nextConfig;
