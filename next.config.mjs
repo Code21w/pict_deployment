d7d2cbadb057c75c68ec8f1672b31b8a1d4e26c6
Code21w <167353588+Code21w@users.noreply.github.com>
2024-08-10 11:29:55 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,20 @@ const nextConfig = {
     return config;
   },
   
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://pict-deployment.vercel.app/" }, // replace this with your actual origin
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ],
+      },
+    ];
+  },
 
   experimental: {
     turbo: {
@@ -26,21 +40,5 @@ const nextConfig = {
     },
   },
 };
-export default nextConfig;
 
-module.exports = {
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  }
-};
+export default nextConfig;
